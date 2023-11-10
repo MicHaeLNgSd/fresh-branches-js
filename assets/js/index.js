9b550b11d56b7c74ed7410b28458242dd18d31e1
Mike <misha.grigorov2020@gmail.com>
2023-11-10 21:58:27 +0200

    variables-operands-hw

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+let num1 = 10;
+let num2 = 20;
+console.log("num1 num2:", num1, num2);
+
+let ex1 = num1 + num2;
+console.log("ex1:", ex1);
+
+let ex2 = ex1 - 50;
+console.log("ex2:", ex2);
+
+let ex3 = ex2 * ex1;
+console.log("ex3:", ex3);
+
+let ex4 = ex3 / 2.687;
+console.log("ex4:", ex4);
+let temp4 = ex3 % 2.687;
+console.log("temp4:", temp4);
+
+let bool1 = !!num1;
+console.log("bool1(num1):", bool1);
+
+let num = 5;
+num += 10;
+num *= 2;
+num -= 4;
+num /= 6;
+console.log("num:", num);
