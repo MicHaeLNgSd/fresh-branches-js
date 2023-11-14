4deaff023edcecb4a42994305da7e21bd336a0b8
Mike <misha.grigorov2020@gmail.com>
2023-11-14 20:38:47 +0200

    scope-test

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// function getSumOfTwoNums(num1, num2) {
+//   if (num1 !== num1 || typeof num1 !== 'number') {
+//     return null;
+//   }
+//   if (num2 !== num2 || typeof num2 !== 'number') {
+//     return null;
+//   }
+//   return num1 + num2;
+// }
+
+(3>2) ? console.log('true') : console.log('false');
