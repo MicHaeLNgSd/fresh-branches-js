828288c68b5bb64f80db4be759a9b3162017304e
Mike <misha.grigorov2020@gmail.com>
2023-11-28 21:03:16 +0200

    errors and start of primitives

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+
+try{	
+  console.log('huhj');
+  const MyFirstError = new Error('U are wrong');
+  console.log(MyFirstError);
+  console.dir(MyFirstError);
+  throw MyFirstError
+  console.log('gg');
+  
+} catch(err){
+  console.log('error ',err)
+}finally {
+  console.log('always')
+}
+console.log('after')
