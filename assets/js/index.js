c2f59a54f4ac2ed3cec7f557e703e845edc5998d
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2023-12-20 15:25:02 +0200

    merge-sort

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const arr = [5, 1, 684, 6, -5500, 6, 158];
+
+function merge(leftArr, rigthArr) {
+  let sortArr = [];
+
+  while (leftArr.length && rigthArr.length) {
+    const smallestNum =
+      leftArr[0] < rigthArr[0] ? leftArr.shift() : rigthArr.shift();
+    sortArr.push(smallestNum);
+  }
+
+  const resultArr = [...sortArr, ...leftArr, ...rigthArr];
+  return resultArr;
+}
+
+function mergeSort(arr = []) {
+  if (arr.length <= 1) return arr;
+  let middleIndex = Math.floor(arr.length / 2);
+
+  const leftArr = arr.slice(0, middleIndex);
+  const rigthArr = arr.slice(middleIndex);
+
+  const leftSortedArr = mergeSort(leftArr)
+  const rigthSortedArr = mergeSort(rigthArr)
+
+  return merge(leftSortedArr, rigthSortedArr)
+}
