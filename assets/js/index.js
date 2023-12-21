01e0ca49266483bc06653ea240d91c6e1fd03b27
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2023-12-21 19:51:04 +0200

    set

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const arr1 = ['aa','bb','ff','hh'];
+const arr2 = ['gg','aa','tt','rr'];
+
+// let temp = new Set([...arr1, ...arr2])
+let allNames = [...new Set([...arr1, ...arr2])]
+
+const testObj = {
+  key1: 'value1',
+  key2: 'value2'
+}
+
+for (const entry of Object.entries(testObj)) {
+  console.log(entry)
+}
+for (const entry of Object.keys(testObj)) {
+  console.log(entry)
+}
