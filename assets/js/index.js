4ce903b456ea76ed83cfce5ccb3704566003fff3
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-01-14 19:58:16 +0200

    RegExp super basic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const regExp1 = new RegExp('qwerty');
+const regExp2 = /^qwert\.y$/i;
+
+const testStr = '02138947012397qwerty2384572qw34905';
+console.log(regExp1.test(testStr));
+console.log(testStr.match(regExp2));
